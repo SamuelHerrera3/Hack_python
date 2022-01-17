03e76313acb54fc4e681d848326753fb9226eafe
Samuel Herrera <Samuelherrera034@gmail.com>
2022-01-16 20:07:23 -0500

    ensayos de seguridad informatica con python

---
+++ infodns.py
@@ -0,0 +1,17 @@
+import dns.resolver
+
+def main():
+	informacion = ['A','AAAA','NS','SOA','MX','MF','MD','TXT']
+	for c in informacion:
+		try:
+			a = dns.resolver.query("achirou.com", c)
+			for q in a:
+				print(q)
+		except:
+			print("No pude obtener la consulta")
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		exit()
---
+++ infoweb.py
@@ -0,0 +1,21 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description="Detector de web")
+parser.add_argument('-t','--target',help="Objetivo")
+parser = parser.parse_args()
+
+def main():
+	if parser.target:
+		try:
+			url = requests.get(url=parser.target)
+			web = dict(url.headers)
+			for x in web:
+				print(x + " : " + web[x])
+		except:
+			print("No se logro llegar al objetivo")
+	else:
+		print("No se encuentra objetivo")
+
+if __name__ == '__main__':
+	main()
